e590e43fd0102acb5d688798c81f2689dda9a531
Emma <doodybrainz@gmail.com>
2017-11-01 17:24:04 -0400

    switch player, add continuous

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component} from 'react';
 import {withRouter} from 'react-router-dom';
 import './App.css';
 import axios from 'axios';
+import Video from './video.js';
 
 class App extends Component {
   constructor() {
@@ -21,17 +22,19 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>m i x t a p e</h1>
+        <h1>mixtape</h1>
+
         <p>{sources.length} tracks</p>
+
         <div className="videos">
           {sources.map((src, i) => {
             return(
               <div key={i} className="video">
-                <div id={i} data-name="frame" className="frame" dangerouslySetInnerHTML={{__html: src.embed.html}}></div>
-                <input id={`${i}-checkbox`}type="checkbox" onClick={this.vidPlaying.bind(this, i)} className="overlay" />
-                <label>{src.title}</label>
+                <Video
+                  id={i}
+                  url={src.source.url}
+                  title={src.title} />
               </div>
-
             );
           })}
         </div>
@@ -39,30 +42,11 @@ class App extends Component {
     );
   }
 
-  vidPlaying(i) {
-    let srcauto = '';
-    const clicked = document.getElementById(i);
-    const checkbox = document.getElementById(i+"-checkbox");
-    const iframe = clicked.innerHTML;
-    const regex = /(src)=["']([^"']*)["']/gi;
-
-    if (checkbox.checked === true) {
-      iframe.replace(regex, function(all, type, value) {
-        srcauto = value += "&autoplay=1";
-      });
-      clicked.innerHTML = `<iframe src="${srcauto}"></iframe>`;
-    } else {
-      iframe.replace(regex, function(all, type, value) {
-        srcauto = value += "&autoplay=0";
-      });
-      clicked.innerHTML = `<iframe src="${srcauto}"></iframe>`;
-    }
-  }
-
   getSrc() {
     const {sources} = this.state;
     const sourceArray = sources.slice();
     let mixtapeChannel = "/mixtape-1509221468";
+
     if (this.props.location.pathname !== '/') {
       mixtapeChannel = this.props.location.pathname;
     }
@@ -73,8 +57,8 @@ class App extends Component {
       res.map((item) => {
         if (item.embed) {
           if (item.embed.html) {
-            sourceArray.push(item)
-            this.setState({ sources: sourceArray})
+            sourceArray.push(item);
+            this.setState({ sources: sourceArray});
           }
         }
       })
---
+++ src/video.js
@@ -0,0 +1,65 @@
+import React, { Component} from 'react';
+import {withRouter} from 'react-router-dom';
+import './App.css';
+import ReactPlayer from 'react-player';
+
+class Video extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playing: false,
+      currentId: this.props.id
+    }
+  }
+
+  render() {
+    const {playing, currentId} = this.state;
+    const {url, title} = this.props;
+
+    return (
+      <div className="media-player">
+        <ReactPlayer
+          height="80%"
+          width="80%"
+          id={`player-${currentId}`}
+          url={url}
+          onEnded={this.onEnd.bind(this, currentId)}
+          playing={playing} />
+
+        <input
+          id={`${currentId}-checkbox`} type="checkbox" onClick={this.vidPlaying.bind(this, currentId)}
+          className="overlay" />
+
+        <label>{title}</label>
+      </div>
+    );
+  }
+
+  vidPlaying(i) {
+    const clicked = document.getElementById(i);
+    const checkbox = document.getElementById(i+"-checkbox");
+
+    if (checkbox.checked === true) {
+      this.setState({ playing: true });
+    } else {
+      this.setState({ playing: false });
+    }
+  }
+
+  onEnd(last) {
+    const next = last + 1;
+    const checkboxlast = document.getElementById(last+"-checkbox");
+    const checkboxnext = document.getElementById(next+"-checkbox");
+    const first = document.getElementById("0-checkbox");
+    checkboxlast.checked = false;
+
+    if (checkboxnext) {
+      checkboxnext.click();
+    } else {
+      first.click();
+    }
+  }
+}
+
+export default withRouter(Video);
